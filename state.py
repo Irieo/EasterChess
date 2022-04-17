e3e82e2d4fe8421588702dac0e268a4469a38707
Irieo <iegor.riepin@b-tu.de>
2022-04-17 21:42:47 +0200

    State class

--- state.py
+++ state.py
@@ -1,42 +1,17 @@
-#!/usr/bin/env python3
-
-from logging import exception
-import os
-import chess.pgn
-
-# https://archive.org/download/KingBase2018/KingBase2018-pgn.zip
-# pgn files in the data folder
-
-# pgn parsing: https://python-chess.readthedocs.io/en/latest/pgn.html
-for fn in os.listdir("data"):
-    pgn = open(os.path.join("data", fn))
-    while 1:
-        try:
-            game = chess.pgn.read_game(pgn)
-            # print(game)
-        except Exception: 
-            break
-        value = {'1/2-1/2':0, '0-1':-1, '1-0':1}[game.headers['Result']]
-        print(value)
-        '''
-        board = game.board()
-        for i, move in enumerate(game.mainline_moves()):
-            board.push(move)
-            # print(i)
-            # print(result)
-            # print(board)
-        exit(0)
-        '''
-    break
+import chess
 
 class State(object): 
-    def __init__(self):
-        self.board = chess.Board()
+    def __init__(self, board=None):
+        if board is None:
+            self.board = chess.Board()
+        else: 
+            self.board = board
 
     def serialize(self):
         # 257 bits according to readme
-        pass
-
+        pp = self.board.shredder_fen()
+        return pp
+        
     def edges(self):
         return list(self.board.legal_moves)
 
@@ -45,4 +20,4 @@ class State(object):
 
 if __name__ == "__main__":
     s = State()
-    print(s.edges()) 
+    print(s.edges())
